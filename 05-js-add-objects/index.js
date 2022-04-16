9f51e6f22f6f492c84364f013f428964699cab51
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-04-17 01:36:24 +0300

    feat: add solution to additional task requirements 2022-04-17 (01:36:24)

--- 05-js-add-objects/index.js
+++ 05-js-add-objects/index.js
@@ -1,3 +1,4 @@
+/*  ----- NECESSARY TASK SOLUTION ---- */
 function add(...array) {
   let result = {};
   array.forEach(el => {
@@ -9,9 +10,79 @@ function add(...array) {
   return result;
 }
 
+// some default examples to check for errors
 const a = { x: 1 };
 const b = { x: 2, y: 2 };
+const c = { a: 4, c: 9, b: 1 };
+const d = { z: 8, b: 7, y: 2 };
 
-console.log(add(a, b, a));
+// console.log(add(a, b, c, d));
 
+/*  ----- ADDITIONAL TASK SOLUTION ---- */
+function intersect(...array) {
+  // check for amount of arguments
+  if (array.length < 1) {
+    return {};
+  } else if (array.length === 1) {
+    return array[0];
+  } else {
+    let result = {};
+    const argKeyArr = [];
+    const argValArr = [];
 
+    // getting all object keys and values to work with
+    array.forEach(el => {
+      argKeyArr.push(Object.keys(el));
+      argValArr.push(Object.values(el));
+    });
+    // set object to check its keys and values for identity
+    const workObjKeys = argKeyArr[0];
+    const workObjVals = argValArr[0];
+
+    // loop through all picked object properties
+    for (let i = 0; i < workObjKeys.length; i++) {
+      let isFieldExist = true;
+      // check current field for identity throughout all other objects
+      for (let j = 1; j < argKeyArr.length; j++) {
+        let isFieldExistTemp = true;
+        if (isFieldExist) {
+          // check for existense of the current key
+          if (isFieldExistTemp) {
+            isFieldExistTemp = argKeyArr[j].includes(workObjKeys[i]);
+          }
+          // if found check for the value equality
+          if (isFieldExistTemp) {
+            const valueIndex = argKeyArr[j].indexOf(workObjKeys[i]);
+            isFieldExistTemp = argValArr[j][valueIndex] === workObjVals[i];
+          }
+          isFieldExist = isFieldExistTemp;
+        }
+      }
+      // if current field the same in each and every object -> place it to result object
+      if (isFieldExist) {
+        result[workObjKeys[i]] = workObjVals[i];
+      }
+    }
+    return result;
+  }
+}
+
+// zero equal props test case
+/* const w = { a: 0, b: 1 };
+const x = { c: 2, a: 1 };
+const y = { d: 3, e: 4, a: 0 };
+const z = { f: 5, g: 6, h: 7, a: 0 }; */
+
+// one equal props test case
+/* const w = { a: 0, b: 1 };
+const x = { c: 2, a: 0 };
+const y = { d: 3, e: 4, a: 0 };
+const z = { f: 5, g: 6, h: 7, a: 0 }; */
+
+// two equal props test case
+/* const w = { a: 0, b: 1 };
+const x = { c: 2, a: 0, b: 1 };
+const y = { b: 1, d: 3, e: 4, a: 0 };
+const z = { b: 1, f: 5, g: 6, h: 7, a: 0 }; */
+
+// console.log(intersect(w, x, y, z));
